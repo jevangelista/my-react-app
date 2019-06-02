3a4044aea3be240c5e9d2e1efd07ed37cf876de6
Jamie Evangelista <j.evangelista88@gmail.com>
2019-06-01 22:22:12 -0700

    updated page 2 link

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Learn to design and code React apps </h1>
+    <p>Complete courses about the best tools and design systems. Prototype and buid apps with React and Swift.</p>
+
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
